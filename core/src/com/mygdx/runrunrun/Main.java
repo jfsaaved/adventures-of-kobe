df186ecd9ed14bce68f12b10445a7e709f97d06d
Julian Saavedra <julian.felipe.saavedra@gmail.com>
2015-07-25 00:37:09 -0400

    Fresh Template

--- core/src/com/mygdx/runrunrun/Main.java
+++ core/src/com/mygdx/runrunrun/Main.java
@@ -5,6 +5,8 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.mygdx.runrunrun.states.GSM;
+import com.mygdx.runrunrun.states.PlayState;
 
 public class Main extends ApplicationAdapter {
 
@@ -12,13 +14,27 @@ public class Main extends ApplicationAdapter {
     public static final int WIDTH = 800;
     public static final int HEIGHT = 400;
 
+    private GSM gsm;
+    private SpriteBatch sb;
+
+
 	@Override
 	public void create () {
+
+        Gdx.gl.glClearColor(0.7f, 0.5f, 0.1f,1);
+
+        sb = new SpriteBatch();
+        gsm = new GSM();
+        gsm.push(new PlayState(gsm));
 
 	}
 
 	@Override
 	public void render () {
+
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        gsm.update(Gdx.graphics.getDeltaTime());
+        gsm.render(sb);
 
 	}
 }
---
+++ core/src/com/mygdx/runrunrun/states/PlayState.java
@@ -0,0 +1,29 @@
+package com.mygdx.runrunrun.states;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+/**
+ * Created by 343076 on 25/07/2015.
+ */
+public class PlayState extends State{
+
+    public PlayState(GSM gsm){
+        super(gsm);
+
+        System.out.println("Hello World!");
+
+    }
+
+    public void handleInput(){
+
+    }
+
+    public void update(float dt){
+
+
+    }
+
+    public void render(SpriteBatch sb){
+
+    }
+}
