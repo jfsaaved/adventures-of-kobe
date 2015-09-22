0cca5b82336a2dd3a90f744e3bce18ec061cd8fe
Julian Saavedra <julian.felipe.saavedra@gmail.com>
2015-09-21 20:20:50 -0400

    September 21, 2015 - Commit 1
    Added menu state (but not used)
    To Town button working
    Proper rect box for Town button
    Adding Town button, when pressed, player goes to Town
    
    Cleaned up PlayState
    
    Item placeholders

---
+++ core/src/com/mygdx/runrunrun/states/MenuState.java
@@ -0,0 +1,30 @@
+package com.mygdx.runrunrun.states;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+
+/**
+ * Created by 343076 on 21/09/2015.
+ */
+public class MenuState extends State {
+
+    public MenuState(GSM gsm){
+        super(gsm);
+    }
+
+    public void handleInput(){
+
+    }
+
+    public void update(float dt){
+
+    }
+
+    public void render(SpriteBatch sb){
+
+    }
+
+    public void shapeRender(ShapeRenderer sr){
+
+    }
+}
