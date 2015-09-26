8f6b15030d1664216d1c33f317d9fb1c0f1bfa6f
Julian Saavedra <julian.felipe.saavedra@gmail.com>
2015-09-25 22:04:21 -0400

    September 25, 2015 - Commit 1
    Main Menu crash on TextImage create
    
    Start at main menu state. On click goes to play state.
    
    Changing amount of items for shop options
    Added bonus functions for Hero
    Added back coin objects
    Functionality test for add health on shop option pick

--- core/src/com/mygdx/runrunrun/states/MenuState.java
+++ core/src/com/mygdx/runrunrun/states/MenuState.java
@@ -3,21 +3,25 @@ package com.mygdx.runrunrun.states;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.mygdx.runrunrun.Main;
+import com.mygdx.runrunrun.ui.TextImage;
 
 /**
  * Created by 343076 on 21/09/2015.
  */
 public class MenuState extends State {
 
+    private TextImage title;
+
     public MenuState(GSM gsm){
         super(gsm);
+
+        title = new TextImage("Run", Main.WIDTH/2, Main.HEIGHT/2,1);
+        title.setTextHide(false);
+
     }
 
     public void handleInput(){
-
-    }
-
-    public void update(float dt){
         if(Gdx.input.justTouched()){
             mouse.set(Gdx.input.getX(), Gdx.input.getY(), 0);
             cam.unproject(mouse);
@@ -27,8 +31,12 @@ public class MenuState extends State {
         }
     }
 
-    public void render(SpriteBatch sb){
+    public void update(float dt){
+        handleInput();
+    }
 
+    public void render(SpriteBatch sb){
+        title.render(sb);
     }
 
     public void shapeRender(ShapeRenderer sr){
