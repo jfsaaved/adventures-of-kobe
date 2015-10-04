e9e5d068032fcb7bdaa9af04623c4a0feda8de1d
Julian Saavedra <julian.felipe.saavedra@gmail.com>
2015-10-03 22:09:34 -0400

    October 3, 2015 - Commit 3
    Transition on MenuState to PlayState
    Polishing: On fall down from flying, and forbid boost on certain conditions such as being on shop
    Reduced shop items to 2
    
    Fixed shop item enums
    
    Coin interaction
    UI clean up

--- core/src/com/mygdx/runrunrun/states/MenuState.java
+++ core/src/com/mygdx/runrunrun/states/MenuState.java
@@ -1,6 +1,8 @@
 package com.mygdx.runrunrun.states;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.mygdx.runrunrun.Main;
@@ -12,10 +14,13 @@ import com.mygdx.runrunrun.ui.TextImage;
 public class MenuState extends State {
 
     private TextImage title;
+    private float transitionVal;
+    private boolean startTransition;
 
     public MenuState(GSM gsm){
         super(gsm);
 
+        startTransition = false;
         title = new TextImage("RUN", Main.WIDTH/2, Main.HEIGHT/2,1);
         title.setTextHide(false);
 
@@ -26,13 +31,28 @@ public class MenuState extends State {
             mouse.set(Gdx.input.getX(), Gdx.input.getY(), 0);
             cam.unproject(mouse);
 
-            gsm.set(new PlayState(gsm,5));
+            startTransition = true;
+            transitionVal = 0f;
+        }
+    }
+
+    private void transition(float dt){
+        if(startTransition){
+
+            transitionVal += 1f * dt;
+
+            if(transitionVal >= 1f){
+                gsm.set(new PlayState(gsm, 5));
+            }
 
         }
     }
 
     public void update(float dt){
+
         handleInput();
+        transition(dt);
+
     }
 
     public void render(SpriteBatch sb){
@@ -45,6 +65,17 @@ public class MenuState extends State {
     }
 
     public void shapeRender(ShapeRenderer sr){
+        Gdx.gl.glEnable(GL20.GL_BLEND);
+        Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
 
+        sr.setProjectionMatrix(cam.combined);
+        sr.begin(ShapeRenderer.ShapeType.Filled);
+
+        sr.setColor(new Color(0,0,0,transitionVal));
+        sr.rect(0,0,Main.WIDTH,Main.HEIGHT);
+
+
+        sr.end();
+        Gdx.gl.glDisable(GL20.GL_BLEND);
     }
 }
