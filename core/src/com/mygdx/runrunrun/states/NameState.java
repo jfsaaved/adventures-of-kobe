2ccadf49a5d46dbb1e449343b033ce9ffde4bec1
Julian Saavedra <julian.felipe.saavedra@gmail.com>
2015-11-27 19:45:01 -0500

    November 27, 2015 - Commit 2
    Implementing NameState
    Added enter and exit to MenuState
    
    Finalizing the game:
    TODO:
    - U.I. for Play
    - Sounds and music
    - Connection to Server using HTTP POST
    - Get ID from server

--- core/src/com/mygdx/runrunrun/states/NameState.java
+++ core/src/com/mygdx/runrunrun/states/NameState.java
@@ -8,6 +8,7 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.Rectangle;
 import com.mygdx.runrunrun.Main;
+import com.mygdx.runrunrun.ui.TextImage;
 
 import java.util.Vector;
 
@@ -23,15 +24,44 @@ public class NameState extends State {
     private TextureRegion fontSheet;
     private Vector<Rectangle> letterBox;
 
+    private TextImage enter;
+    private TextImage back;
+    private TextImage exit;
+    private Rectangle enterRect;
+    private Rectangle backRect;
+    private Rectangle exitRect;
+
     public NameState(GSM gsm){
         super(gsm);
 
+        /**
+         * Transition variables for the parent
+         */
         enterTransition = true;
         enterTransitionVal = 1f;
         getEnterTransitionValHelper = 1f;
-
         exitTransition = false;
 
+        /**
+         * Text images for enter and exit
+         */
+        enter = new TextImage("ENTER",0,0,0.5f);
+        back = new TextImage("BACK",0,0,0.5f);
+        exit = new TextImage("EXIT",0,0,0.5f);
+        enter.setTextHide(false);
+        back.setTextHide(false);
+        exit.setTextHide(false);
+        enter.update((Main.WIDTH / 2 + 370), enter.getHeight() * 6);
+        back.update((Main.WIDTH / 2 + 370 - (45/2)), back.getHeight() * 4) ;
+        exit.update((Main.WIDTH/2 + 370 - (45/2)), exit.getHeight() * 2);
+
+        enterRect = new Rectangle(Main.WIDTH / 2 + 260,enter.getHeight() * 5,enter.getWidth(),enter.getHeight());
+        backRect = new Rectangle(Main.WIDTH / 2 + 260,back.getHeight() * 3,back.getWidth(),back.getHeight());
+        exitRect = new Rectangle(Main.WIDTH / 2 + 260,exit.getHeight(),exit.getWidth(),exit.getHeight());
+
+        /**
+         * Below are the letter boxes images
+         */
         fontSheet = new TextureRegion(Main.resource.getAtlas("assets").findRegion("nameSheet"));
         bg = new TextureRegion(Main.resource.getAtlas("assets").findRegion("tiled_bg"));
         bg2 = bg;
@@ -59,15 +89,28 @@ public class NameState extends State {
 
     @Override
     protected void handleInput() {
-
+        if(Gdx.input.justTouched() && !exitTransition && !enterTransition){
+            mouse.set(Gdx.input.getX(), Gdx.input.getY(), 0);
+            cam.unproject(mouse);
+            if(enterRect.contains(mouse.x,mouse.y)){
+                Main.sounds.playSound("select");
+                exitTransition = true;
+                exitTransitionVal = 0f;
+            }else if(exitRect.contains(mouse.x,mouse.y)){
+                Main.sounds.playSound("select");
+                exitTransition = true;
+                exitTransitionVal = 0f;
+            }
+        }
     }
 
     @Override
     protected void update(float dt) {
 
         handleInput();
-        onEnterTransition(dt);
         onExitTransition(dt);
+        onEnterTransition(dt);
+
 
         if(currentBGX < bg.getRegionWidth()){
             currentBGX++;
@@ -86,24 +129,15 @@ public class NameState extends State {
 
         sb.draw(fontSheet,Main.WIDTH/2 - fontSheet.getRegionWidth()/2, -180);
 
+        enter.render(sb);
+        back.render(sb);
+        exit.render(sb);
+
         sb.end();
     }
 
     public void shapeRender(ShapeRenderer sr){
-
         super.shapeRender(sr);
-
-        sr.setProjectionMatrix(cam.combined);
-        sr.begin(ShapeRenderer.ShapeType.Line);
-
-        if(!enterTransition || !exitTransition) {
-            sr.setColor(Color.WHITE);
-            for (Rectangle box : letterBox) {
-                sr.rect(box.getX(), box.getY(), box.getWidth(), box.getHeight());
-            }
-        }
-
-        sr.end();
     }
 
 
